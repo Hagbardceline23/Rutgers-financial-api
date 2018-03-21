94a07c6dc423ef49f3facce84c99b553a3a1d8c0
Fabian Flores <ffflores1@outlook.com>
2018-03-21 18:09:01 -0400

    Added error checking to login and signup features. Started firebase watchlist logic.

--- assets/js/financial.js
+++ assets/js/financial.js
@@ -12,6 +12,7 @@ const AlphaBatch = "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTE
 const AlphaSuffix = "&apikey=" + AlphaAPIKEY;
 const AlphaTS = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=";
 const AlphaTSSuffix = AlphaSuffix;
+const MinPassLength = 7;
 
 // https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&outputsize=full&apikey=demo
 
@@ -54,6 +55,72 @@ $(document).ready(() => {
   // initialize database parent objects
   function initdb() {
     console.log("in initdb()");
+  }
+
+  // -----------------------------------------------------------------------------
+  // addUserToDb() takes in three parameters. If the user node does not already
+  //  exist in the database, the node is added based on email.
+  //
+  function addUserToDb(fname, lname, email) {
+    var userRef;
+
+    console.log("in addUserToDb(): " + fname, lname, email);
+    // create users reference
+    userRef = database.ref().child("users");
+    // if users node does not exist create
+    database.ref().once("value", (snapshot) => {
+      if (snapshot.child("users").exists()) {
+          console.log("users child exists under root");
+      } else {
+          // create users node by pushing
+        // database.ref().push("users");
+        console.log("users node does not exist under root");
+      }
+   });
+
+    // if users/email child does not exist create it
+
+    // if users/email/watchlist node does not exist, create it
+
+    // if users/email/portfolio does not exist, create it
+  }
+
+  // -----------------------------------------------------------------------------
+  // hasAlpha() checks if a string has at least one alphabetic character, lower
+  // or upper case
+  //
+  function hasAlpha(str) {
+    return str.match(/[a-z]/i);
+  }
+
+  // -----------------------------------------------------------------------------
+  // hasNum() checks if a string has at least one numeric character
+  //
+  function hasNum(str) {
+    return str.match(/\d+/g);
+  }
+
+  // -----------------------------------------------------------------------------
+  // validatePassword() checks for password validity, the password must
+  // be greater than MinPassLength, have alpha characters and at least one digit
+  //
+  function validatePassword(pswd) {
+    if (pswd.length >= MinPassLength && hasAlpha(pswd) && hasNum(pswd)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // -----------------------------------------------------------------------
+  // validateEmail() checks if an email is valid
+  // source code for regular expression:
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript/1373724#1373724
+  //
+  function validateEmail(email) {
+    var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+
+    return re.test(email);
   }
 
 
@@ -181,7 +248,7 @@ $(document).ready(() => {
     cardh5.text(data["1. symbol"]).
            attr("stock-sym", data["1. symbol"]);
     htmlText = "Price: " + numeral(data["2. price"]).format("$0,0.00") + "<br />";
-    htmlText += "Volume: " + data["3. volume"] + "<br />";
+    htmlText += "Volume: " + numeral(data["3. volume"]).format("0,0") + "<br />";
     htmlText += "Company: <a href=" + currentWatchRow.website + " target=\"_blank\">" + currentWatchRow.companyName + "</a><br />";
     htmlText += "About: " + currentWatchRow.description + "<br />";
 
@@ -189,6 +256,7 @@ $(document).ready(() => {
             append(addToWatchBtn);
 
     stockDiv.append(cardh5, cardBody);
+    $("#stock-input").show();
     $("#stock-ticker-content").append(stockDiv);
   }
 
@@ -319,8 +387,9 @@ $(document).ready(() => {
     console.log("in stock-ticker() ");
     $("#financial-text").empty();
 
-    // clear stock-input field
-    $("#stock-input").val("");
+    // hide clear stock-input field
+    $("#stock-input").hide().
+                      val("");
 
     event.preventDefault();
     buildBatchURL(stockSymbol, "ticker");
@@ -378,10 +447,18 @@ $(document).ready(() => {
 
     // sign in
     promise = auth.signInWithEmailAndPassword(email, pswrd);
-    promise.catch((error) => console.log(error.message));
+    promise.catch((error) => {
+      console.log(error.message);
+      loginSuccess = false;
+      $("#nonExistentUser").addClass("text-info font-weight-normal").
+      text("Enter correct info or sign up please.");
+    });
 
     if (loginSuccess) {
       $("#appEmail, #appPassword").val("");
+      $("#nonExistentUser").empty();
+      $("#pLogin").modal("hide");
+    } else {
       $("#pLogin").modal("hide");
     }
   }
@@ -390,33 +467,81 @@ $(document).ready(() => {
   // signupRoutine() signs up a user to the app
   //
   function signupRoutine(ev) {
-    var fname = $("#appFirstname").val(),
-        lname = $("#appLastname").val(),
+    var fname = $("#appFirstName").val(),
+        lname = $("#appLastName").val(),
         email = $("#signupEmail").val(),
         pswrd = $("#signupPassword").val(),
         confirmPswrd = $("#confirmPassword").val(),
         auth = firebase.auth(),
         signupSuccess = true,
+        validEmail = false,
+        validPswd = false,
+        pswdsMatch = false,
+        htmlText = "",
         promise;
-
-    // error check email and password check later
-
-    // function validateEmail(email)
-    // {
-    // var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-    // return re.test(email);
-    // }
 
     ev.preventDefault();
     console.log("in signupRoutine()");
 
-    // sign up
-    promise = auth.createUserWithEmailAndPassword(email, pswrd);
-    promise.catch((error) => console.log(error.message));
+    // check email validity
+    if (validateEmail(email)) {
+      console.log("valid email entered");
+      validEmail = true;
+    } else {
+      console.log("invalid email");
+      htmlText = "Please enter valid email.<br />";
+      $("#signupEmail, #signupPassword, #confirmPassword").val("");
+      $("#signup-error").addClass("text-danger font-weight-bold").
+                        html(htmlText);
+      $("#signupEmail").val("");
+      validEmail = false;
+    }
+
+    // check password validity
+    if (validatePassword(pswrd)) {
+      console.log("valid password entered");
+      validPswd = true;
+    } else {
+      console.log("invalid password");
+      $("#signupPassword, #confirmPassword").val("");
+      htmlText += "Please enter valid password. Must be at least " +
+                  MinPassLength +
+                  " characters long and have at least one digit and one alphabetic character.<br />";
+      $("#signup-error").addClass("text-danger font-weight-bold").
+                        html(htmlText);
+      validPswd = false;
+    }
+
+    // check if passwords match
+    if (pswrd === confirmPswrd) {
+      console.log("passwords match");
+      pswdsMatch = true;
+    } else {
+      console.log("passwords do not match");
+      $("#signupPassword, #confirmPassword").val("");
+      htmlText += "Passwords do not match. Please enter them again.<br />";
+      $("#signup-error").addClass("text-danger font-weight-bold").
+                        html(htmlText);
+      pswdsMatch = false;
+    }
+
+    // TODO: check if email already exists in database
+
+    signupSuccess = validEmail && validPswd && pswdsMatch
+                    ? true
+                    : false;
 
     if (signupSuccess) {
-      $("#appFirstname, #appLastname, #signupEmail, #signupPassword, #confirmPassword").val("");
+      promise = auth.createUserWithEmailAndPassword(email, pswrd);
+      promise.catch((error) => console.log(error.message));
+      $("#signup-error").removeClass("text-danger font-weight-bold").
+                        empty();
+      $("#appFirstName, #appLastName, #signupEmail, #signupPassword, #confirmPassword").val("");
+      $("#nonExistentUser").empty();
       $("#pSignup").modal("hide");
+      addUserToDb(fname, lname, email);
+    } else {
+      $("#pSignup").modal("show");
     }
   }
 
@@ -433,17 +558,21 @@ $(document).ready(() => {
   //
   firebase.auth().onAuthStateChanged((firebaseUser) => {
     if (firebaseUser) {
-      console.log("Logged in User is: " + JSON.stringify(firebaseUser));
+      // console.log("Logged in User is: " + JSON.stringify(firebaseUser));
       $("#btnLogout").removeClass("d-none");
       $("#modalLogin").addClass("d-none");
-      $("#loggedInUser").text(firebaseUser.email);
+      $("#modalSignup").addClass("d-none");
+      $("#loggedInUser").addClass("font-weight-bold text-primary mr-1").
+                        html("Welcome, " + firebaseUser.email);
       userEmail = firebaseUser.email;
       userAuthenticated = true;
     } else {
       console.log("Not logged in.");
       $("#btnLogout").addClass("d-none");
       $("#modalLogin").removeClass("d-none");
-      $("#loggedInUser").empty();
+      $("#modalSignup").removeClass("d-none");
+      $("#loggedInUser").removeClass("font-weight-bold text-primary mr-1").
+                        empty();
       userEmail = "";
       userAuthenticated = false;
     }
